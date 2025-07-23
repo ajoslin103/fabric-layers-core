d922d44e6520631a3a73b1d24098b0d61eb77c3b
Al Joslin <allen.joslin@gmail.com>
2025-07-23 08:13:26 -0400

    pre "npm i fabric@5.3.0-browser"

---
+++ src/types/fabric-pure-browser.d.ts
@@ -0,0 +1,7 @@
+// https://github.com/fabricjs/fabric.js/issues/5735#issuecomment-2049157791 
+
+/// <reference types="fabric" />
+
+declare module 'fabric-pure-browser' {
+	export * from 'fabric';
+}
---
+++ typescript-migration/TYPESCRIPT-NOTES.md
@@ -0,0 +1,4 @@
+adding types for fabric.js
+
+https://github.com/fabricjs/fabric.js/issues/5735#issuecomment-2049157791
+
