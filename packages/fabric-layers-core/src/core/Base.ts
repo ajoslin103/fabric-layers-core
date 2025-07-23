8d3d514d2c62075b11788752cd48a0c12290eae8
Al Joslin <allen.joslin@gmail.com>
2025-07-22 22:12:21 -0400

    **Core Components First**

--- packages/fabric-layers-core/src/core/Base.ts
+++ packages/fabric-layers-core/src/core/Base.ts
@@ -1,51 +1,28 @@
 import { EventEmitter2 } from 'eventemitter2';
 
-/**
- * Interface for base options that can be passed to any component
- */
 export interface BaseOptions {
   [key: string]: any;
 }
 
-/**
- * Base class that all components extend from
- * Provides common functionality like event handling and property access
- */
 class Base extends EventEmitter2 {
-  /**
-   * Internal options storage
-   */
   protected _options: BaseOptions;
 
-  /**
-   * Constructor for the Base class
-   * @param options - Configuration options
-   */
   constructor(options?: BaseOptions) {
-    super(options);
+    super();
     this._options = options || {};
-    Object.assign(this, options);
+    Object.assign(this, this._options);
   }
-  
-  /**
-   * Set a property value
-   * @param key - Property name
-   * @param value - Property value
-   * @returns - This instance for chaining
-   */
+
   set(key: string, value: any): this {
-    this._options[key] = value;
+    (this as any)[key] = value;
     return this;
   }
 
-  /**
-   * Get a property value
-   * @param key - Property name
-   * @returns - Property value
-   */
   get(key: string): any {
-    return this._options[key];
+    return (this as any)[key];
   }
+
+  // EventEmitter2 methods are already typed by the base class
 }
 
 export default Base;
