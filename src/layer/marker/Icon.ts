124cb54ed8eded6c46912aa28080e1dad6ece77d
Al Joslin <allen.joslin@gmail.com>
2025-07-26 10:23:57 -0400

    icons

--- src/layer/marker/Icon.ts
+++ src/layer/marker/Icon.ts
@@ -3,20 +3,20 @@ import { ICON } from '../../core/Constants';
 
 export interface IconOptions extends fabric.IImageOptions {
   // Add any additional icon options here
+  src?: string | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement;
 }
 
 export class Icon extends fabric.Image {
   public defaults: typeof ICON;
   protected _options?: IconOptions;
 
-  constructor(options?: IconOptions) {
-    super(options);
+  constructor(imageSource: string | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement, options?: IconOptions) {
+    super(imageSource, options);
     this.defaults = { ...ICON };
-    Object.assign({}, this.defaults);
-    Object.assign({}, this._options);
+    this._options = options;
   }
 }
 
-export const icon = (options?: IconOptions): Icon => new Icon(options);
+export const icon = (imageSource: string | HTMLImageElement | HTMLCanvasElement | HTMLVideoElement, options?: IconOptions): Icon => new Icon(imageSource, options);
 
 export default Icon;
